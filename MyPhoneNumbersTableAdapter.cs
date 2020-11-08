296e0d474e2525e8f2cba29109f75340837d7d6e
TheodoraBaciu <72906483+TheodoraBaciu@users.noreply.github.com>
2020-11-08 18:24:31 +0200

    Add files via upload

---
+++ MyPhoneNumbersTableAdapter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace Baciu__Lab5.PhoneNumbersDataSetTableAdapters
+{
+    public partial class PhoneNumbersTableAdapter
+    {
+        private bool ClearBeforeFill;
+        private SqlConnection Connection;
+
+        public PhoneNumbersTableAdapter(String connectionString)
+        {
+            this.ClearBeforeFill = true;
+            this.Connection = new
+           System.Data.SqlClient.SqlConnection(connectionString);
+        }
+    }
+}
